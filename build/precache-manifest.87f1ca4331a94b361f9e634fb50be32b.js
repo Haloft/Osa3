4bd96c75b9ddf2c05ceb53d1c4d1c7c306591d74
Arto Halonen <c8haar00@students.oamk.fi>
2019-12-29 02:16:47 +0200

    3.11 muutokset

---
+++ build/precache-manifest.87f1ca4331a94b361f9e634fb50be32b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053f3ac5999854f6eedf0329c6ab4af1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aebeb041ca08bd656c76",
+    "url": "/static/css/main.af30585e.chunk.css"
+  },
+  {
+    "revision": "0936524af70262dd875d",
+    "url": "/static/js/2.e6a3551d.chunk.js"
+  },
+  {
+    "revision": "6a1db4d7caa458c322c562453ccbcfaf",
+    "url": "/static/js/2.e6a3551d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aebeb041ca08bd656c76",
+    "url": "/static/js/main.db94b220.chunk.js"
+  },
+  {
+    "revision": "57d8af95778078760a0e",
+    "url": "/static/js/runtime-main.283795fd.js"
+  }
+]);
